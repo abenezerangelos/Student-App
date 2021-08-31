120310c961c415c578d679905b56516aae3c4070
AnthBoss <anthony.salvione@wsu.edu>
2021-09-01 01:51:36 +0200

    defined major in the Class class

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ class Class(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     coursenum = db.Column(db.String(3))  
     title = db.Coulumn(db.String(3))
+    major = db.Column(db.String(20),db.ForeignKey('major.name'))
 
     def __repr__(self):
         return '<Class id: {} - coursenum: {}>'.format(self.id,self.coursenum)
