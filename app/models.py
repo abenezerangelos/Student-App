38d55fd2cb8aed93af1d1c4f4398f89b5aa3de9d
AnthBoss <anthony.salvione@wsu.edu>
2021-09-01 01:53:26 +0200

    Added the getTitle function

--- app/models.py
+++ app/models.py
@@ -5,10 +5,10 @@ class Class(db.Model):
     coursenum = db.Column(db.String(3))  
     title = db.Coulumn(db.String(3))
     major = db.Column(db.String(20),db.ForeignKey('major.name'))
-
     def __repr__(self):
         return '<Class id: {} - coursenum: {}>'.format(self.id,self.coursenum)
-
+    def getTitle(self):
+        return self.title
 class.Major(db.Model): 
     name = db.Column(db.String(20), primary_keys=True)
     department = db.Column(db.String(150))
