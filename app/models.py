b8a21346f184a22bb83921a75312fbac70c64a90
AnthBoss <anthony.salvione@wsu.edu>
2021-09-01 02:02:23 +0200

    Typo - line 6 of models.py

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 class Class(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     coursenum = db.Column(db.String(3))  
-    title = db.Coulumn(db.String(3))
+    title = db.Column(db.String(3))
     major = db.Column(db.String(20),db.ForeignKey('major.name'))
     def __repr__(self):
         return '<Class id: {} - coursenum: {}>'.format(self.id,self.coursenum)
