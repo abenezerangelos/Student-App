ac6a9d920b594d0bc99042754c51d6bbd6a881b4
AnthBoss <anthony.salvione@wsu.edu>
2021-09-01 19:38:50 -0700

    Implemented user registration

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,22 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app,db
 
-from app.forms import ClassForm
-from app.models import Class, Major
+from app.forms import ClassForm, RegistrationForm
+from app.models import Class, Major, Student
 
 @app.before_first_request
 def initDB(*args, **kwargs):
     db.create_all()
+    if Major.query.count() == 0:
+        majors = [{'name':'CptS','department':'School of EECS'},
+                  {'name':'SE','department':'School of EECS'},
+                  {'name':'EE','department':'School of EECS'},
+                  {'name':'ME','department':'Mechanical Engineering'},
+                  {'name':'MATH','department':'Mathematics'}  ]
+        for t in majors:
+            db.session.add(Major(name=t['name'], department=t['department']))
+        db.session.commit()
+
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
@@ -24,3 +34,15 @@ def createclass():
         flash('Class "' + newClass.major + '-' + newClass.coursenum + '" is created')
         return redirect(url_for('index'))
     return render_template('create_class.html', form = form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        student = Student(username=form.username.data, email=form.email.data, firstname=form.firstname.data, lastname=form.lastname.data, address=form.address.data)
+        student.set_password(form.password.data)
+        db.session.add(student)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('index'))
+    return render_template('register.html', form = form)
